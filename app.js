e901084fba69ec30696a8c9de2ec6c825421be2d
ChrisFSotelo <sotelolancheroscristianferney@gmail.com>
2025-08-26 23:46:59 -0500

    "Agregar nuevas rutas de API para usuarios: búsqueda por ID y parámetros. Implementación de '/api/v1/users/:id/' para obtener usuarios por ID y '/api/v1/search' para búsqueda por parámetros (id, nombre). Código reorganizado para mejorar claridad."

--- app.js
+++ app.js
@@ -9,15 +9,40 @@ const path = require('path')
 //puerto
 const port = 8000;
 
-app.get('/api/v1/users', function(req,res){
-  const currentDir = path.join(__dirname, 'src/data/');
-  const currentFile = `${currentDir}data.json`;
+const currentDir = path.join(__dirname, 'src/data/');
+const currentFile = `${currentDir}data.json`;
 
+// GET USER ALL
+
+app.get('/api/v1/users', function(req,res){
   //parsear la respuesta
   const rawData = fs.readFileSync(currentFile);
   const response = JSON.parse(rawData);
   
     res.json({data: response});
 })
+
+// GET USER BY ID
+app.get('/api/v1/users/:id/', function(req,res){
+  const id = req.params.id;
+
+  const rawData = fs.readFileSync(currentFile);
+  const jsonFile =  JSON.parse(rawData);
+  const response = jsonFile.find(u => + u.id === +id);
+  res.json({data: response});
+});
+
+// SEARCH USER BY ID
+app.get('/api/v1/search', function(req,res){
+  const id = req.query.id;
+  const name = req.query.name;
+  const rawData = fs.readFileSync(currentFile);
+  const jsonFile =  JSON.parse(rawData);
+  const response = jsonFile.filter(u => {
+    if (+u.id === +id) return true;
+  })
+  res.json({data: response});
+});
+
 
 app.listen(port);
